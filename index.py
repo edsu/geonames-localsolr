0da586d992423bf344c2a96e38a89e8ad006d791
Ed Summers <ehs@pobox.com>
2009-01-21 18:01:57 -0500

    added state indexing

--- index.py
+++ index.py
@@ -15,13 +15,14 @@ for row in codecs.open(sys.argv[1], encoding='utf8'):
     count += 1
     row = row.strip()
     p = row.split("\t")
-    id, name, lat, lng, country = p[0], p[1], p[4], p[5], p[8]
+    id, name, lat, lng, country, state = p[0], p[1], p[4], p[5], p[8], p[10]
     alt_name = p[3].split(',')
-    s.add(id=id, name=name, lat=lat, lng=lng, country=country,
+
+    s.add(id=id, name=name, lat=lat, lng=lng, state=state, country=country,
             alt_name=alt_name)
     if count * 1000 == 0:
         s.commit()
-    print "[%s] %s %s [%s]" % (count, name, country, id)
+    print "[%s] %s %s  %s [%s]" % (count, name, state, country, id)
 
 s.commit()
     
